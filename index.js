fc1398e3566d6df0c792b766805b0bc3c4a674aa
Manuel Meneses <mannuel.menneses@gmail.com>
2017-11-10 17:50:51 -0600

    Body-parser

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 // ROUTES
 // ==============================================
